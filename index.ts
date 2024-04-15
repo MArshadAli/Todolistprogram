5e3fcb8e5cb9d4a33cff04034f289a8497d8f1f1
MArshadAli <159759372+MArshadAli@users.noreply.github.com>
2024-04-15 14:18:21 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+console.log("Welcome to Todo List")
+let todos = [];
+let condition = true;
+
+while(condition)
+{
+let addTask = await inquirer.prompt(
+    [{
+        name:"todo",
+        type: "input",
+        message:"What do you want to add in your todos"
+    },
+    {
+        name:"addMore",
+        type:"confirm",
+        message:"Do you want to add more in your todo",
+        default:"false"
+    }]);
+
+todos.push(addTask.todo);
+condition= addTask.addMore
+console.log(todos)
+}
